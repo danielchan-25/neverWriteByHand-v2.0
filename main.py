6b4a5b4a3156ba1f681cc5ceb1eb93e991915fae
danielchan-25 <alexchan0325@gmail.com>
2024-04-22 22:28:43 +0800

    添加网页输入功能。

--- main.py
+++ main.py
@@ -1,51 +1,62 @@
 import os
 import random
-from PIL import ImageFont
-from PIL import Image
-from PIL import ImageDraw
 import time
+from PIL import ImageFont, Image, ImageDraw
+from flask import Flask, request, render_template, send_file
+
+app = Flask(__name__, template_folder="static")
+
+SIZE = 4  # 整齐度
+FONT_PATH = r"fonts/"
+SAVE_PATH = r"output/"
 
 
 # 获取文字内容
 def get_content(txt_path):
-    with open(txt_path, 'r', encoding='UTF-8') as f:
+    with open(txt_path, "r", encoding="UTF-8") as f:
         content = f.read()
-    f.close()
     return content
 
 
-def main(content, ttf_path, save_path, size):
-    font = ImageFont.truetype(ttf_path, 25)  # 设置字体
+def generate_image(size, content, ttf_path, save_path):
+    FONT = ImageFont.truetype(ttf_path, 25)  # 设置字体
 
-    lenstr = len(content)
-    flag = 0
+    LENGSTR = len(content)
+    FLAG = 0
 
-    while flag < lenstr:
-        img = Image.open('background.png')
+    while FLAG < LENGSTR:
+        img = Image.open("background.png")
         draw = ImageDraw.Draw(img)
         for i in range(28):
             for j in range(38):
-                if flag >= lenstr:
+                if FLAG >= LENGSTR:
                     break
-                if content[flag] == '\n':
-                    flag += 1
+                if content[FLAG] == "\n":
+                    FLAG += 1
                     break
                 draw.text((70 + random.random() * size / 2 + 25 * j, 83 + random.random() * size + i * 48),
-                          content[flag], (0, 0, 0),
-                          font=font)
-                flag += 1
-            if flag >= lenstr:
+                          content[FLAG], (0, 0, 0),
+                          font=FONT)
+                FLAG += 1
+            if FLAG >= LENGSTR:
                 break
-        img.save(save_path + str(int(time.time())) + ".png")
-    print("success!")
+        img_path = os.path.join(save_path, f"{int(time.time())}.png")
+        img.save(img_path)
+    return img_path
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        user_input = request.form["content"]
+        img_path = generate_image(size=SIZE, content=user_input,
+                                  ttf_path=os.path.join(FONT_PATH, "STLITI.TTF"),
+                                  save_path=SAVE_PATH
+                                  )
+        return send_file(img_path, mimetype="image/png")
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
-    size = 4  # 整齐度
-    ttf_path = ".\\fonts\\"
-    save_path = ".\\output\\"
-    content = get_content(txt_path='content.txt')
-
-    for font in os.listdir(ttf_path):
-        if font.endswith(".TTF") or font.endswith(".ttf"):
-            main(content=content, ttf_path=f"{ttf_path}{font}", save_path=save_path, size=4)
+    app.run(debug=True, host="0.0.0.0", port=5001)
